20d7b03e8156384bf18f243c6b9a2d6e6319476c
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-07-09 15:04:39 +0100

    Database created, seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def run_seed
+ 64.times do 
+    stars = (1..5).to_a.sample
+    restaurant = Restaurant.new(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_address,
+      phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+      category: Faker::Restaurant.type 
+      )
+      restaurant.save
+      puts "#{restaurant.id} seeded deeply in da base"
+  end
+end
+    
+    
+puts "Clearing the database"
+Restaurant.destroy_all
+puts "Database Cleared!"
+puts "Procedding to seed......"
+run_seed
+
+puts "Seeded !!!!!"
+    
+    
+    
