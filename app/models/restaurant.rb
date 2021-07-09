bf34ed7c434815df169fa6fc961597241eb1e7b7
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-07-09 18:10:24 +0100

    Fully functional single model wit simple_forms

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: {case_sensitive: false}    
+  validates :address,  presence: true, uniqueness: {case_sensitive: false}    
+  validates :category, presence: true  
+
 end
